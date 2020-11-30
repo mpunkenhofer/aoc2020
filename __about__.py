9a90ace8a2ed769530db3b27a2d01e0cf3346ee2
Mathias Punkenhofer <mpunkenhofer@gmail.com>
2020-11-30 11:55:15 +0100

    chore: project setup

---
+++ __about__.py
@@ -0,0 +1,16 @@
+# Mathias Punkenhofer
+# code.mpunkenhofer@gmail.com
+#
+
+__all__ = ["__title__", "__summary__", "__uri__", "__version__", "__author__", "__email__", "__license__"]
+
+__title__ = "AdventOfCode2020"
+__summary__ = "This is a collection of my solutions for Advent of Code 2019 (https://adventofcode.com/2020)"
+__uri__ = "https://github.com/mpunkenhofer/aoc2020"
+
+__version__ = "0.0.0"
+
+__author__ = "Mathias Punkenhofer"
+__email__ = "code.mpunkenhofer@gmail.com"
+
+__license__ = "MIT"
